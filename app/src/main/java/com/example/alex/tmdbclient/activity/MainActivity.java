9dbc7a30db001fdbf2a942d2d6abdea6eb20fe64
alex <asytnyk0@gmail.com>
2017-05-02 14:51:05 +0300

    Create the request

--- app/src/main/java/com/example/alex/tmdbclient/activity/MainActivity.java
+++ app/src/main/java/com/example/alex/tmdbclient/activity/MainActivity.java
@@ -1,15 +1,45 @@
 package com.example.alex.tmdbclient.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.example.alex.tmdbclient.R;
+import com.example.alex.tmdbclient.model.Movie;
+import com.example.alex.tmdbclient.model.MoviesResponse;
+import com.example.alex.tmdbclient.rest.ApiClient;
+import com.example.alex.tmdbclient.rest.ApiInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String API_KEY = "741ad1c19f76c6d8446f966eaacc930c";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+
+        Call<MoviesResponse> call = apiService.getTopRatedMovies(API_KEY);
+        call.enqueue(new Callback<MoviesResponse>() {
+            @Override
+            public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
+                List<Movie> movies = response.body().getResults();
+                Log.d(TAG, "Number of movies received: " + movies.size());
+            }
+
+            @Override
+            public void onFailure(Call<MoviesResponse> call, Throwable t) {
+                Log.e(TAG, t.toString());
+            }
+        });
     }
 }
